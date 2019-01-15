a138160b905257b96b6cffeefdcd128fd4ab374e
snake_eater4526 <41698614+snakeeater4526@users.noreply.github.com>
2019-01-15 10:28:18 +0100

    Update 01_read.py

--- clever_card_kit/01_read.py
+++ clever_card_kit/01_read.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import SimpleMFRC522
 import mysql.connector
@@ -7,12 +9,23 @@ reader = SimpleMFRC522.SimpleMFRC522()
 print("Hold a tag near the reader")
 
 try:
+	db = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='rfid')
     while True:
         id, text = reader.read()
         print(id) # badge_id
         print(text) # badge_content
-        # Se connecter à la base de données
+        
+        cursor = db.cursor()
+        
+        cursor.execute("SELECT max(request_id) from request_log")
+        request_id = cursor.fetch()
+        if(request_id == null):
+			request_id = 1
+		else:
+			request_id += 1
 
+		#sql = 'insert into request_log(request_id, device_id, ip_from, badge_id, badge_content, datetime) values(%s, %s, %s,  %s, %s, now())'
+		
 finally:
     print("cleaning up")
     GPIO.cleanup()
