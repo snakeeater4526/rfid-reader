8adfeb96e96c4f862ca468a861023001553ed349
snake_eater4526 <41698614+snakeeater4526@users.noreply.github.com>
2019-01-15 10:34:48 +0100

    Update 01_read.py

--- clever_card_kit/01_read.py
+++ clever_card_kit/01_read.py
@@ -9,7 +9,7 @@ reader = SimpleMFRC522.SimpleMFRC522()
 print("Hold a tag near the reader")
 
 try:
-	db = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='rfid')
+    db = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='rfid')
     while True:
         id, text = reader.read()
         print(id) # badge_id
@@ -20,10 +20,11 @@ try:
         cursor.execute("SELECT max(request_id) from request_log")
         request_id = cursor.fetch()
         if(request_id == null):
-			request_id = 1
-		else:
-			request_id += 1
+            request_id = 1
+        else:
+            request_id += 1
 
+        print(request_id)
 		#sql = 'insert into request_log(request_id, device_id, ip_from, badge_id, badge_content, datetime) values(%s, %s, %s,  %s, %s, now())'
 		
 finally:
