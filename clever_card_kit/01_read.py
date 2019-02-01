e0e32ecfe20452969be18a625f05eb26a6e64bd6
snake_eater4526 <41698614+snakeeater4526@users.noreply.github.com>
2019-02-01 15:06:58 +0100

    Update 01_read.py

--- clever_card_kit/01_read.py
+++ clever_card_kit/01_read.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import RPi.GPIO as GPIO
 import SimpleMFRC522
+import time
+import socket
 
 reader = SimpleMFRC522.SimpleMFRC522()
 
@@ -12,13 +15,24 @@ try:
         id, text = reader.read()
         print(id)
         print(text)
-
         file = open("badge_id_and_name.txt","w")
         file.write('{0}'.format(id))
         file.write("\n")
         file.write('{0}'.format(text))
         file.close()
-                
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        print(s.getsockname()[0])
+        ip_from = s.getsockname()[0]  
+        s.close()
+        
+        os.system('curl -X GET "http://10.1.1.147?device_id=3&ip_from='+ip_from$
+
+        time.sleep(5)
 finally:
     print("cleaning up")
     GPIO.cleanup()
+
+       s.close()
+         
