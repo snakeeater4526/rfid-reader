f42199bca5581a92e58e4cab37c8bb12cb7fd93b
snake_eater4526 <41698614+snakeeater4526@users.noreply.github.com>
2019-02-07 14:51:13 +0100

    Update rfid.sh

--- rfid.sh
+++ rfid.sh
@@ -131,30 +131,43 @@ reader = SimpleMFRC522.SimpleMFRC522()
 
 print("Hold a tag near the reader")
 
-try:
-    while True:
-        id, text = reader.read()
-        print(id)
-        print(text)
-        file = open("badge_id_and_name.txt","w")
-        file.write('{0}'.format(id))
-        file.write("\n")
-        file.write('{0}'.format(text))
-        file.close()
+while True:
+        try:
+                id, text = reader.read()
+                print(id)
+                print(text)
+                print(type(text))
+                file = open("badge_id_and_name.txt","w")
+                file.write('{0}'.format(id))
+                file.write("\n")
+                file.write('{0}'.format(text))
+                file.close()
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        print(s.getsockname()[0])
-        ip_from = s.getsockname()[0]  
-        s.close()
-        os.system('curl -X GET "http://${master}?device_id=3&ip_from='+ip_from+'&badge_id='+str(id)+'&badge_content='+text.replace(" ", '%20')+'"')
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.connect(("8.8.8.8", 80))
+                print(s.getsockname()[0])
+                ip_from = s.getsockname()[0]  
+                s.close()
+                os.system('curl -X GET "http://${master}?device_id=3&ip_from='+ip_from+'&badge_id='+str(id)+'&badge_content='+text.replace(" ", '%20')+'"')
 
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(4, GPIO.OUT)
+                GPIO.output(4, GPIO.HIGH)
+                time.sleep(2)
+                GPIO.output(4, GPIO.LOW)
 
+        except:
+                os.system('curl -X GET "http://${master}?device_id=3&ip_from='+ip_from+'&badge_id='+str(id)+'&badge_content=null"')
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(4, GPIO.OUT)
+                GPIO.output(4, GPIO.HIGH)
+                time.sleep(2)
+                GPIO.output(4, GPIO.LOW)
 
-        time.sleep(5)
-finally:
-    print("cleaning up")
-    GPIO.cleanup()
+        finally:
+                print("cleaning up")
+                GPIO.cleanup()
+
 EOF
 
 sudo bash -c 'cat > /etc/rc.local' << EOF
